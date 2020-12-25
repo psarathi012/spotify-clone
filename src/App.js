8667deb0aa8c449b0542ab6931a3f9600c720071
psarathi012 <45533706+psarathi012@users.noreply.github.com>
2020-12-25 20:47:20 +0530

    intial commit
    rsponse taken

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect } from "react";
+import Login  from './components/Login';
+import { getTokenFromResponse } from "./components/spotify";
 
 function App() {
+  
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    console.log(hash,"helllllo");
+    window.location.hash = "";
+    
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app"> 
+    <Login/>
+  
     </div>
   );
 }
